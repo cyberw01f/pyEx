795d601d1b8b3315fc2e94a1a65527a1e1d59895
cyberw0lf <dubey.abhineet@gmail.com>
2017-05-24 04:27:50 -0700

    adding total file Counter

--- fileStats/fileCount.py
+++ fileStats/fileCount.py
@@ -4,3 +4,22 @@
 # found in the LICENSE file.
 
 #under construction
+
+import os
+
+videoFiles = ['mkv','mp4','avi','MP4','MKV','AVI','ogv','OGV']
+audioFiles = ['mp3','aac','m4a','flac','ogg','MP3','AAC','M4A','FLAC','OGG']
+
+while(fileLoc!='exit'):
+    fileLoc=raw_input("Enter location to search[exit to terminate]: ")
+    if fileLoc=='exit':
+        return 0
+    else:
+        for (dir, _, files) in os.walk(fileLoc):
+            for f in files:
+                if f[-3:] in videoFiles:
+                    vfiles=vfiles+1
+                elif f[-3:] in audioFiles:
+                    afiles=afiles+1
+
+print "Number of Video File = ",vfiles,"Number of Audio Files = ",afiles
