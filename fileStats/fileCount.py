d8e1011d605a5cae72ecd64a60bf7bced86bfab3
cyberw0lf <dubey.abhineet@gmail.com>
2017-05-25 03:31:26 -0700

    updated fileCount.py and added some test files

--- fileStats/fileCount.py
+++ fileStats/fileCount.py
@@ -9,17 +9,28 @@ import os
 
 videoFiles = ['mkv','mp4','avi','MP4','MKV','AVI','ogv','OGV']
 audioFiles = ['mp3','aac','m4a','flac','ogg','MP3','AAC','M4A','FLAC','OGG']
+pdfFiles = ['pdf','PDF']
+fileLoc=""
 
-while(fileLoc!='exit'):
-    fileLoc=raw_input("Enter location to search[exit to terminate]: ")
+def counter(fileLoc):
+    vfiles=0
+    afiles=0
+    pfiles=0
+    for (dir, _, files) in os.walk(fileLoc):
+        for f in files:
+            if f[-3:] in videoFiles:
+                vfiles=vfiles+1
+            elif f[-3:] in audioFiles:
+                afiles=afiles+1
+            elif f[-3:] in pdfFiles:
+                pfiles=pfiles+1
+    print "Number of Video File = ",vfiles,"\nNumber of Audio Files = ",afiles,"\nNumber of PDF = ",pfiles
+    return
+
+
+while 1:
+    fileLoc=raw_input("Enter location to search [exit to quit]\n>>")
     if fileLoc=='exit':
-        return 0
+        break
     else:
-        for (dir, _, files) in os.walk(fileLoc):
-            for f in files:
-                if f[-3:] in videoFiles:
-                    vfiles=vfiles+1
-                elif f[-3:] in audioFiles:
-                    afiles=afiles+1
-
-print "Number of Video File = ",vfiles,"Number of Audio Files = ",afiles
+        counter(fileLoc)
