db3f708481c0221a1597d6746d3ce3f638c9cb42
cyberw0lf <dubey.abhineet@gmail.com>
2017-05-25 14:23:29 -0700

    fileCount.py now prints in color

--- fileStats/fileCount.py
+++ fileStats/fileCount.py
@@ -7,16 +7,17 @@
 
 import os
 import math
+from termcolor import colored
 
 videoFiles = ['mkv','mp4','avi','MP4','MKV','AVI','ogv','OGV']
 audioFiles = ['mp3','aac','m4a','flac','ogg','MP3','AAC','M4A','FLAC','OGG']
-pdfFiles = ['pdf','PDF']
+documentFiles = ['pdf','PDF','doc','xls','odt','odp','rtf','txt']
 fileLoc=""
 
 def counter(fileLoc):
     vfiles=0
     afiles=0
-    pfiles=0
+    dfiles=0
     total=1
     for (dir, _, files) in os.walk(fileLoc):
         for f in files:
@@ -24,13 +25,16 @@ def counter(fileLoc):
                 vfiles=vfiles+1
             elif f[-3:] in audioFiles:
                 afiles=afiles+1
-            elif f[-3:] in pdfFiles:
-                pfiles=pfiles+1
-    total=vfiles+afiles+pfiles
-    print ("Number of Video File =",vfiles,"\nNumber of Audio Files = ",afiles,"\nNumber of PDF = ",pfiles,"\nTotal number of files = ",total)
-    print ("=" *math.ceil(vfiles*10/total))
-    print ("=" *math.ceil(afiles*10/total))
-    print ("=" *math.ceil(pfiles*10/total))
+            elif f[-3:] in documentFiles:
+                dfiles=dfiles+1
+    total=vfiles+afiles+dfiles
+    print ("Number of Video File =",vfiles,"\nNumber of Audio Files = ",afiles,"\nNumber of documents = ",dfiles,"\nTotal number of files = ",total)
+    vBar = colored('|','red')
+    aBar = colored('|','green')
+    dBar = colored('|','blue')
+    print (vBar * math.ceil(vfiles*10/total))
+    print (aBar * math.ceil(afiles*10/total))
+    print (dBar * math.ceil(dfiles*10/total))
     return
 
 while 1:
