c9d271020da206f878c09e6fd5752087c5c033ee
cyberw0lf <dubey.abhineet@gmail.com>
2017-05-25 18:06:03 -0700

    fileCount.py now works for any directory, counts total size also

--- fileStats/fileCount.py
+++ fileStats/fileCount.py
@@ -4,7 +4,6 @@
 # found in the LICENSE file.
 
 #under construction
-
 import os
 import math
 from termcolor import colored
@@ -14,28 +13,50 @@ audioFiles = ['mp3','aac','m4a','flac','ogg','MP3','AAC','M4A','FLAC','OGG']
 documentFiles = ['pdf','PDF','doc','xls','odt','odp','rtf','txt']
 fileLoc=""
 
+def getFileSize(fileName): #function made for future use
+    fileSize=os.path.getsize(fileName)
+    return fileSize
+
+def GetHumanReadable(size,precision=2):
+    suffixes=['B','KB','MB','GB','TB']
+    suffixIndex = 0
+    while size > 1024 and suffixIndex < 4:
+        suffixIndex += 1 #increment the index of the suffix
+        size = size/1024.0 #apply the division
+    return "%.*f%s"%(precision,size,suffixes[suffixIndex])
+
 def counter(fileLoc):
-    vfiles=0
-    afiles=0
-    dfiles=0
-    total=1
+    vfiles=afiles=dfiles=vSize=aSize=dSize=totalFiles=totalSize=0
+    vtotal=atotal=dtotal=0
     for (dir, _, files) in os.walk(fileLoc):
         for f in files:
             if f[-3:] in videoFiles:
                 vfiles=vfiles+1
+                vSize=getFileSize(os.path.join(dir, f))
+                vtotal=vtotal+vSize
             elif f[-3:] in audioFiles:
                 afiles=afiles+1
+                aSize=getFileSize(os.path.join(dir, f))
+                atotal=atotal+aSize
             elif f[-3:] in documentFiles:
                 dfiles=dfiles+1
-    total=vfiles+afiles+dfiles
-    print ("Number of Video File =",vfiles,"\nNumber of Audio Files = ",afiles,"\nNumber of documents = ",dfiles,"\nTotal number of files = ",total)
-    vBar = colored('|','red')
-    aBar = colored('|','green')
-    dBar = colored('|','blue')
-    print (vBar * math.ceil(vfiles*10/total))
-    print (aBar * math.ceil(afiles*10/total))
-    print (dBar * math.ceil(dfiles*10/total))
+                dSize=getFileSize(os.path.join(dir, f))
+                dtotal=dtotal+dSize
+    totalFiles=vfiles+afiles+dfiles
+    if totalFiles==0:
+        totalFiles=1
+    print("There are Video Files : {v} of total size : {s}".format(v=vfiles,s=GetHumanReadable(vtotal,2)))
+    print("There are Audio Files : {v} of total size : {s}".format(v=afiles,s=GetHumanReadable(atotal,2)))
+    print("There are Documents : {v} of total size : {s}".format(v=dfiles,s=GetHumanReadable(dtotal,2)))
+    vBar = colored('>>','red')
+    aBar = colored('>>','green')
+    dBar = colored('>>','blue')
+    print (vBar * math.ceil(vfiles*10/totalFiles))
+    print (aBar * math.ceil(afiles*10/totalFiles))
+    print (dBar * math.ceil(dfiles*10/totalFiles))
     return
+
+
 
 while 1:
     fileLoc=input("Enter location to search [exit to quit]\n>>")
